33c4c85c2c046efc2d201c332bb8f8569d9eb776
nobleOn3 <nob16001@byui.edu>
2019-04-09 02:29:09 -0600

    adding the functionality to search for an image by color

--- public/thank_you.js
+++ public/thank_you.js
@@ -7,8 +7,8 @@ function searchByColor() {
 		console.log("Back from the server with:");
 		console.log(data);
 
-		$("#ulImages").append(`<li>${data.name}</li>`);
-		$("#ulImages").append(`<li>${data.image_file}</li>`);
+		$("#ulImages").append(`<li>#data.name</li>`);
+		$("#ulImages").append(`<li>#data.image_file</li>`);
 		$("#ulImages").append("<li>item1</li>");
 	});
 }
