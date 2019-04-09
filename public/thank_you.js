fac191bc6116ba0f1f517fafd3a7fbd37983454e
nobleOn3 <nob16001@byui.edu>
2019-04-09 02:32:52 -0600

    adding the functionality to search for an image by color

--- public/thank_you.js
+++ public/thank_you.js
@@ -7,8 +7,15 @@ function searchByColor() {
 		console.log("Back from the server with:");
 		console.log(data);
 
-		$("#ulImages").append(`<li>#data.name</li>`);
-		$("#ulImages").append(`<li>#data.image_file</li>`);
+		for(var i = 0; i < data.list.length; i++) {
+			var image = data.list[i];
+
+			console.log(image);
+
+		}
+
+		$("#ulImages").append("<li>#data.name</li>");
+		$("#ulImages").append("<li>#data.image_file</li>");
 		$("#ulImages").append("<li>item1</li>");
 	});
 }
